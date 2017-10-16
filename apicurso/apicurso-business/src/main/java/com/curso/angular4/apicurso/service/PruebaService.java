1b4cd8ac3c333ba5c9d95b6885cbadf9fc60cb8a
david.molina <david.molina@p-externos.iecisa.com>
2017-10-16 18:52:07 +0200

    eliminar prueba

--- apicurso/apicurso-business/src/main/java/com/curso/angular4/apicurso/service/PruebaService.java
+++
@@ -1,9 +0,0 @@
-package com.curso.angular4.apicurso.service;
-
-import com.curso.angular4.apicurso.FileUploadJson;
-
-public interface PruebaService {
-	
-	FileUploadJson devolverMensaje(String nombre);
-
-}
